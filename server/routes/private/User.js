1e85178b1e4511b3967c608f17cb6ebe349ddc78
Amr006 <2021170374@cis.asu.edu.eg>
2023-08-14 18:29:21 +0300

    deploy on render

--- server/routes/private/User.js
+++ server/routes/private/User.js
@@ -17,18 +17,10 @@ const ResetLimiter = rateLimit({
 	// store: ... , // Use an external store for more precise rate limiting
 })
 
-const createAccountLimiter = rateLimit({
-	windowMs: 60 * 60 * 1000, // 1 hour
-	max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
-	message:
-		'Too many accounts created from this IP, please try again after an hour',
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
 
 
 
-router.post("/register", createAccountLimiter ,authControllers.register);
+router.post("/register" ,authControllers.register);
 
 router.get("/user/verify/:userId/:uniqueString", authControllers.verify);
 
