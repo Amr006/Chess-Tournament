e4704f122d8ea7d8ef39e39f3188cf8ae1765854
Amr006 <2021170374@cis.asu.edu.eg>
2023-08-14 18:41:33 +0300

    deploy on render

--- server/server.js
+++ server/server.js
@@ -35,15 +35,15 @@ const corsOptions = {
   exposedHeaders: ['set-cookie'],
 };
 
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-	// store: ... , // Use an external store for more precise rate limiting
-})
+// const limiter = rateLimit({
+// 	windowMs: 15 * 60 * 1000, // 15 minutes
+// 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+// 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// 	// store: ... , // Use an external store for more precise rate limiting
+// })
 
-app.use(limiter)
+// app.use(limiter)
 
 app.use(express.json()) //can remove
 app.use(express.urlencoded({ extended: true }));
